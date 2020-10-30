3b59a1bac9cd269725b39f4858225d500eefb447
Andy LI <54015423+Andy-LI2019@users.noreply.github.com>
2020-10-30 21:40:06 +0800

    Update rm_comments.py

--- rm_comments.py
+++ rm_comments.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 
+
 def main(path):
     """
 
@@ -9,53 +10,66 @@ def main(path):
     :return:
     """
     # print("dir:" + path)
-    new_dir=path[:path.rfind("\\")]+path[path.rfind("\\"):]+"_new"
+    new_dir = path[:path.rfind("\\")] + path[path.rfind("\\"):] + "_new"
     if os.path.exists(new_dir):
         shutil.rmtree(new_dir)
-    shutil.copytree(path,new_dir)
+    shutil.copytree(path, new_dir)
     for root, dirs, files in os.walk(new_dir):
         # print("***")
         # print(files)
         for name in files:
             # print(name)
-            if name.endswith(r".py"):   # only handle the .py files
-                trim_file(os.path.join(root,name))
-
+            if name.endswith(r".py"):  # only handle the .py files
+                trim_file(os.path.join(root, name))
 
 
 def trim_file(path):
     """
 
     """
-    content=[]
-    with open(path,"r",encoding="utf-8") as f:
-        lines=f.readlines()
-        flag=1
+    content = []
+    with open(path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        flag = 1
         for line in lines:
-            if line.strip().startswith("#"): # lines start with #
+            s = line.strip()
+
+            if s == '"""' or s == "'''":
+                flag *= -1
+            elif s.startswith(r"'''") and s.endswith(r"'''"):
                 pass
-            elif line.startswith("\n"):   # null lines
+            elif s.startswith(r'"""') and s.endswith('"""'):
+                pass
+            elif s.startswith(r'"""') or s.endswith('"""') or s.startswith(r"'''") or s.endswith(
+                    "'''"):  # lines in """    """
+                flag *= -1
+            elif flag == -1:
+                pass
+
+            elif s.startswith("#"):  # lines start with #
+                pass
+            elif line.startswith("\n"):  # null lines
                 pass
             elif hash_line(line)['res']:  # line with # in the middle
                 content.append(hash_line(line)['new_line'])
-            elif line.strip().startswith(r'"""'): # lines in """    """
-                flag*=-1
-            elif flag==-1:
-                pass
             else:
                 content.append(line)
-    with open(path,'w+',encoding='utf-8') as f:
+    with open(path, 'w+', encoding='utf-8') as f:
         f.writelines(content)
         print("{} update complete".format(path))
 
     return
-def hash_line(line):
-    res=False
-    new_line=""
-    if '#' in line:
-        comment=line[line.find("#"):]
-        if "\'" not in comment and '\"' not in comment:  # check the "#" belong to strings or not
-            res=True
-            new_line=line[:line.find("#")]+"\n"
 
-    return {"res":res,"new_line":new_line}
+
+def hash_line(line):
+    res = False
+    new_line = ""
+    if '#' in line:
+        comment = line[line.find("#"):]
+        if "\'" not in comment and '\"' not in comment:  # check the "#" belong to strings or not
+            res = True
+            new_line = line[:line.find("#")] + "\n"
+
+    return {"res": res, "new_line": new_line}
+
+
